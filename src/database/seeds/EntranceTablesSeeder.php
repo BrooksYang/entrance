ea2597ace0cb0bb49ebffcb221df961771fc6105
BrooksYang <yangchangqing@easy2study.net>
2017-08-28 16:09:44 +0800

    sync permission for test role

--- src/database/seeds/EntranceTablesSeeder.php
+++ src/database/seeds/EntranceTablesSeeder.php
@@ -541,8 +541,12 @@ class EntranceTablesSeeder extends Seeder
             ],
         ]);
 
-        // Sync permissions for admin
+        // Sync permissions for super admin
         $admin = $role::first();
         $admin->permissions()->sync([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]);
+
+        // Sync permissions for test role
+        $test = $role::find(2);
+        $test->permissions()->sync([25, 31]);
     }
 }
